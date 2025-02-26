fed9429ccdc3731624f7a799a292c6e7f530dc9d
Rahul Singh <thesincererahul@gmail.com>
2025-02-26 13:21:46 +0530

    Added complete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Grid from './components/Grid/Grid'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      hello
+      <Grid numberOfCards={9} />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,19 @@
-function Card() {
+import Icon from "../Icon/Icon";
+import "./Card.css";
+
+function Card({ onPlay, player, index, gameEnd }) {
+    
+    let icon = <Icon />
+
+    if(player == "X") {
+        icon = <Icon name={"cross"} />
+    } else if(player == "O") {
+        icon = <Icon name={"circle"} />
+    }
+
     return (
-        <div>
-            <span></span>
+        <div className="card" onClick={() => !gameEnd && player == "" && onPlay(index)}>
+            {icon}
         </div>
     );
 }
